888456d93323c230b6ca83ec7e3bb0642bf233e8
Morgan Morales-Howard <mmoraleshoward@gmail.com>
2023-05-01 17:45:14 -0500

    completion of task list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.getElementById("createtaskform").addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildTasks(e.target.newtaskdescription.value);
+  }
+  )
+})
+
+function buildTasks (toDo) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener('click', removeButton)
+  btn.textContent = 'X';
+  p.textContent = `${toDo} `;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector('#tasks').appendChild(p);
+}
+
+
+function removeButton (e) {
+  e.target.parentNode.remove();
+}
